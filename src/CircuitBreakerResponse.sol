a88d4b5ee2bbafbe1ff25bd4bd3d3b27e4fe4562
Idle0x <140549997+Idle0x@users.noreply.github.com>
2025-12-28 17:50:47 +0100

    Update CircuitBreakerResponse.sol

--- src/CircuitBreakerResponse.sol
+++ src/CircuitBreakerResponse.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.20;
 
 contract CircuitBreakerResponse {
-    event CircuitBreakerTriggered(string message, bytes data);
+    event CircuitBreakerTriggered(
+        string message, 
+        int256 ethPrice,
+        int256 usdcPrice, 
+        uint128 liquidity
+    );
 
-    function breakCircuit(bytes[] calldata data) external {
-        emit CircuitBreakerTriggered("CRITICAL: Solvency Event Detected", data[0]);
+    function breakCircuit(bytes calldata payload) external {
+        (int256 ethPrice, int256 usdcPrice, uint128 liquidity) = 
+            abi.decode(payload, (int256, int256, uint128));
+
+        emit CircuitBreakerTriggered(
+            "CRITICAL: Solvency Event Detected", 
+            ethPrice, 
+            usdcPrice, 
+            liquidity
+        );
     }
 }
