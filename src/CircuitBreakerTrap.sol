1fcaefefaf2020f5b7a249fc851e5465fd6318aa
Idle0x <140549997+Idle0x@users.noreply.github.com>
2026-03-19 20:06:01 +0100

    Update CircuitBreakerTrap.sol

--- src/CircuitBreakerTrap.sol
+++ src/CircuitBreakerTrap.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Manual Interfaces to save gas
+import "drosera-contracts/interfaces/ITrap.sol";
+
 interface AggregatorV3Interface {
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80);
 }
@@ -10,9 +11,9 @@ interface IUniswapV3Pool {
     function liquidity() external view returns (uint128);
 }
 
-contract CircuitBreakerTrap {
-    
-    // Hardcoded Addresses (Gas Saving)
+contract CircuitBreakerTrap is ITrap {
+
+    // Hardcoded Addresses
     address constant ETH_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
     address constant USDC_FEED = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
     address constant POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
@@ -23,7 +24,7 @@ contract CircuitBreakerTrap {
     uint256 constant LIQUIDITY_DROP_THRESHOLD = 15; 
     uint256 constant ORACLE_TIMEOUT = 3600;         
 
-    function collect() external view returns (bytes memory) {
+    function collect() external view override returns (bytes memory) {
         (, int256 ethPrice, , uint256 updateEth, ) = AggregatorV3Interface(ETH_FEED).latestRoundData();
         (, int256 usdcPrice, , uint256 updateUsdc, ) = AggregatorV3Interface(USDC_FEED).latestRoundData();
         uint128 poolLiquidity = IUniswapV3Pool(POOL).liquidity();
@@ -37,21 +38,23 @@ contract CircuitBreakerTrap {
         return abi.encode(ethPrice, usdcPrice, poolLiquidity);
     }
 
-    function shouldRespond(bytes[] calldata data) external pure returns (bool, bytes memory) {
-        if (data.length < 2 || data[0].length == 0 || data[1].length == 0) {
-            return (false, bytes(""));
-        }
+    function shouldRespond(bytes[] calldata data) external pure override returns (bool, bytes memory) {
+        if (data.length == 0 || data[0].length == 0) return (false, bytes(""));
+        if (data.length < 2 || data[1].length == 0) return (false, bytes(""));
 
         (int256 currEth, int256 currUsdc, uint128 currLiq) = abi.decode(data[0], (int256, int256, uint128));
         (int256 prevEth, , uint128 prevLiq) = abi.decode(data[1], (int256, int256, uint128));
 
+        // Math safety: prevent division by zero
+        if (prevEth <= 0 || prevLiq == 0) return (false, bytes(""));
+
         int256 ethDiff = currEth > prevEth ? currEth - prevEth : prevEth - currEth;
-        
+
         bool isVolatile = (ethDiff * 100 / prevEth) > ETH_VOLATILITY_THRESHOLD;
         bool isDepeg = currUsdc < USDC_DEPEG_LIMIT;
-        
+
         bool isLiquidityDrain = false;
-        if (prevLiq > 0 && currLiq < prevLiq) {
+        if (currLiq < prevLiq) {
              uint256 liqDrop = uint256(prevLiq - currLiq) * 100 / uint256(prevLiq);
              isLiquidityDrain = liqDrop > LIQUIDITY_DROP_THRESHOLD;
         }
